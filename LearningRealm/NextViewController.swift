dd2602917aa6ceb9384dd4377e8762d07ec30b28
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2021-03-02 22:21:32 +0900

    UI作成

---
+++ LearningRealm/NextViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NextViewController.swift
+//  LearningRealm
+//
+//  Created by 渕一真 on 2021/03/02.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+    @IBOutlet weak var todoTitleTF: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func didTapSendButton(_ sender: UIButton) {
+    }
+}
